7eda5cc387a7dd27b3fa57542626e00f608bfb80
nooroongy <haerammy@gmail.com>
2022-04-04 22:45:49 +0900

    modify sing in page

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,13 +1,17 @@
 import { FB_AUTH } from '../components/_firebase'
+import '../css/auth.css'
 
 const Auth = () => {
     function onClickSignInPopBtn() {
         FB_AUTH.signInWithGoogle(false);
     }
 
-    return (<>
-        {/* <button onClick={onClickSignInBtn}>login</button> */}
-        <button onClick={onClickSignInPopBtn}>sign In With Google</button>
-    </>)
+    return (<div className='auth__wrap'>
+        <span className='auth__logo'>Good Sleep</span>
+        <div className='auth__btn_wrap'>
+            <button className='auth__btn' onClick={onClickSignInPopBtn}>sign In<br />With Google</button>
+            <button className='auth__btn' >show demo</button>
+        </div>
+    </div>)
 }
 export default (Auth);
